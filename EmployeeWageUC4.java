80f9e6d419f54c132b681aead7c8a0fbf012a7ad
Darpan Madlani <Darpan@proclink.com>
2021-11-19 20:20:04 +0530

    part time feature implement with switch-case statement

---
+++ EmployeeWageUC4.java
@@ -0,0 +1,32 @@
+public class EmployeeWageUC4 {
+
+	//Constant
+	static final int IS_PRESENT = 1;
+	static final int IS_PART_TIME = 2;
+	static final int SALARY_PER_HOUR = 20;
+
+	public static void main(String[] args) {
+		//Variables
+		int empCheck;
+		empCheck = (int)(Math.floor(Math.random() * 10) % 3);
+		int empHr = 0;
+
+		switch(empCheck) {
+			case IS_PRESENT :
+				empHr = 8;
+			break;
+
+			case IS_PART_TIME :
+				empHr=4;
+			break;
+
+			default :
+				empHr=0;
+			break;
+		}
+
+		double empSalary = empHr * SALARY_PER_HOUR;
+		
+		System.out.println("Emp Wage : "+empSalary);
+	}
+}
