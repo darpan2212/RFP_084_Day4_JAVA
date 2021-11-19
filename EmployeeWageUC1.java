8f3eb55221c7529ea98883b682326505e2ff774b
Darpan Madlani <Darpan@proclink.com>
2021-11-19 19:41:22 +0530

    Checking employee attendance with Math.random() method

---
+++ EmployeeWageUC1.java
@@ -0,0 +1,16 @@
+public class EmployeeWageUC1 {
+
+	public static void main(String[] args) {
+		//Constant
+		int IS_PRESENT = 1;
+		
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+
+		
+		if (empCheck == IS_PRESENT)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
+	}
+
+}
