021517076e64f2bfaa8dd4e309b70668e6e7616f
Darpan Madlani <Darpan@proclink.com>
2021-11-19 20:01:38 +0530

    Calculate salary of the employee per day

---
+++ EmployeeWageUC2.java
@@ -0,0 +1,17 @@
+public class EmployeeWageUC2 {
+
+	public static void main(String[] args) {
+		//Constant
+		int IS_PRESENT = 1;
+		int SALARY_PER_HOUR = 20;
+		//Variables
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		int workingHour = 0;
+		if (empCheck == IS_PRESENT) {
+			workingHour=8;
+		}
+		double empSalary = workingHour * SALARY_PER_HOUR;
+		
+		System.out.println("Employee has earned "+empSalary+" dollar today.");
+	}
+}
